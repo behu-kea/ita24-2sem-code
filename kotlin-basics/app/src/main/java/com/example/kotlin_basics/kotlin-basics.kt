a57842c08026afd6e60e82467aa63d9721ebff84
Benjamin Hughes <behu@kea.dk>
2025-01-27 13:25:40 +0100

    asd

--- kotlin-basics/app/src/main/java/com/example/kotlin_basics/kotlin-basics.kt
+++ kotlin-basics/app/src/main/java/com/example/kotlin_basics/kotlin-basics.kt
@@ -3,15 +3,31 @@ package com.example.kotlin_basics
 fun main() {
     val numbers: List<Int> = listOf(1,2,3,4,5);
 
-    val transformedArray = transformArray(numbers);
-    println(transformedArray)
+    //val transformedArray = transformArray(numbers);
+    //println(transformedArray)
 
-    val str: String = "2"
-    val number: Int = 2
-    if(str == number) {
-        print(1)
+    numberCypherEncoder("ben");
+}
+
+fun numberCypherEncoder(inputString: String): String {
+    val alphabet = "abcdefghijklmnopqrstuvxyzæøå";
+    var decodedString = ""
+    // 1. forloop
+    for (i in 0 until inputString.length) {
+        //println(i)
+        val currentCharacter: Char = inputString.get(i)
+        println(currentCharacter);
+        val charAlphabeatOrder = alphabet.indexOf(currentCharacter) + 1
+
+        if(i == inputString.length - 1) {
+            decodedString = decodedString + charAlphabeatOrder;
+        } else {
+            decodedString = decodedString + charAlphabeatOrder + ";";
+        }
     }
 
+    println(decodedString)
+    return ""
 }
 
 fun transformArray(array: List<Int>): MutableList<Int> {
